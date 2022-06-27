1348382e07f827e27ebeb268dab775ef9baa9562
alopalka <d3bian@mopalka.idl.pl>
2022-06-27 17:34:59 +0200

    Added .gitignore

--- social_media/notforyoureyes.py
+++ social_media/notforyoureyes.py
@@ -1,3 +1,3 @@
 values={
-    "SECRET_KEY":'django-insecure-9of@0=ctqj#-old1+r5-xw)23(sm0wc7lm)y62*=_-11sj5t8m',
+    "SECRET_KEY":'django-insecure-9of@0=ctqj#-old1+r5-xw)23(sm0wc7lm)y62*=_-11sj5t8m'
 }
