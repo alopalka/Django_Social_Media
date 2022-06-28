090db151f1f35e79b56d6bf6aeed58c68ca5cf28
alopalka <d3bian@mopalka.idl.pl>
2022-06-28 22:12:14 +0200

    Created first model in posts/models.py

--- social_media/posts/models.py
+++ social_media/posts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Post(models.models):
+
+    id=models.AutoField()
+    user=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    post_text=models.CharField(max_length=2000)
+    created_at=models.DateTimeField(auto_now_add=True)
+    likes=models.IntegerField()
+
+    def __str__(self):
+        return "{} : {} : {}".format(self.user,self.likes,self.created_at)
+        
+
